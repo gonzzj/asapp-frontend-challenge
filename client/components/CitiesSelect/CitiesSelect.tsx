ac08223644621267c640d9f8f3c296b88368710d
gonzzj <gonzalojusid@gmail.com>
2022-03-20 18:12:17 -0300

    Added CitiesSelect component + Installed lodash & MUI

---
+++ client/components/CitiesSelect/CitiesSelect.tsx
@@ -0,0 +1,11 @@
+import styles from './CitiesSelect.module.scss';
+
+const CitiesSelect = () => {
+  return (
+    <div className={styles.select}>
+      Get started by editing
+    </div>
+  );
+};
+
+export default CitiesSelect;
